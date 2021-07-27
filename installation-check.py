9208151f23e394eda341b2e6e0d75bde6e903b92
aashish-2096 <aashish2096@gmail.com>
2021-07-27 07:09:16 +0530

    Credentials Update

--- installation-check.py
+++ installation-check.py
@@ -1,13 +1,15 @@
 from selenium import webdriver 
 from selenium.webdriver.common.keys import Keys
 
+
+username = str(input("Enter Email"))
+password  = str(input("Enter Password"))
+
 driver = webdriver.Chrome() 
 # get google.co.in 
 driver.get('https://www.linkedin.com')
 
 
-username= "aashish2096@gmail.com"
-password = "nokiax200"
 #LinkedIn login
 u_name = driver.find_element_by_name('session_key')
 u_name.send_keys(username)
@@ -29,16 +31,27 @@ search_query.send_keys(Keys.RETURN)
 #Filtering profile Links
 elems = driver.find_elements_by_css_selector(".yuRUbf [href]")
 links = [elem.get_attribute('href') for elem in elems]
-
 print(len(links))
+with open('profile.txt','w') as f:
+    f.write('\n'.join(links))
+    f.close()
 print(links[0])
-driver.get(links[0])
-print(driver.page_source())
 
 #Filtering WebPage Links
 
-elems = driver.find_elements_by_css_selector(".AaVjTc [href]")
-links = [elem.get_attribute('href') for elem in elems]
 
-print(len(links))
-print(links[3])
+# elems = driver.find_elements_by_css_selector(".AaVjTc [href]")
+# links = [elem.get_attribute('href') for elem in elems]
+
+# print(len(links))
+# print(links[3])
+driver.get(links[0])
+val_source = driver.page_source
+sect = driver.find_elements_by_class_name ("pv-profile-section__section-info section-info")
+print(sect)
+
+# print(val_source)
+# with open('source.txt','w') as fs:
+#     fs.write(val_source)
+#     fs.close()
+#print(driver.page_source)
